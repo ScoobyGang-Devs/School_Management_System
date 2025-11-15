0ca98a61b13a2e637d68afc65ec339394df50245
rathnayakesu <shanukaushantha2004@gmail.com>
2025-11-15 12:48:38 +0530

    added student and teachers attendence card

---
+++ frontend/src/pages/Attendance/AttendenceCard.jsx
@@ -0,0 +1,38 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Users, LayoutList } from "lucide-react"; // Changed icon for class context
+import { Link } from 'react-router-dom';
+
+function AttendenceCard({catogery}) {
+  
+  // The link path must be RELATIVE to its parent route!
+  // Parent route path: /admin/students/classes/:gradeLevel
+  // Child route path: :classId (which is className here)
+//   const linkPath = `${className}`; 
+
+  return (
+    <Card className="w-[300px] hover:shadow-lg hover:border-primary transition-all duration-200">
+       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        {/* Combines grade and class name */}
+        <CardTitle className="text-3xl font-bold">{catogery}</CardTitle>
+        <LayoutList className="h-6 w-6 text-muted-foreground" /> 
+      </CardHeader>
+        <CardFooter>
+        <Link className="w-full">
+          <Button className="w-full" variant="secondary">
+            View Attendence
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default AttendenceCard;
--- frontend/src/pages/administration/SchoolWideAttendance.jsx
+++ frontend/src/pages/administration/SchoolWideAttendance.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import AttendenceCard from '../Attendance/AttendenceCard'
 
 const SchoolWideAttendance = () => {
   return (
-    <div>SchoolWideAttendance</div>
+    <div className="flex flex-col sm:flex-row gap-6 w-full justify-center items-center sm:items-stretch py-4">
+
+      <AttendenceCard catogery="Student"/>
+      <AttendenceCard catogery="Teachers"/>
+    </div>
+    
   )
 }
 
