df8a2115fb0a2cce9fe44b59e828814f09aba120
Adithya Paranavithana <nimsaraadithya642@gmail.com>
2025-11-17 21:18:15 +0530

    student grade avergae api view

--- backend/term_test/urls.py
+++ backend/term_test/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('subjectwise-marks/', SubjectwiseMarkListCreateView.as_view(), name='subjectwise-mark-list'),
     path('subjectwise-marks/<int:pk>/', SubjectwiseMarkListDetailView.as_view(), name='subjectwise-mark-detail'),
     path('marks/<int:grade>/<str:classname>/<int:term>/', GradeClassWiseResultsView.as_view(), name='subjectwise-marks-filtered-by-class'),
+    path('average/grade/<int:grade>/term/<int:term>' , StudentGradeAverageView.as_view(), name='average-grade-mark'),
 
 ]
