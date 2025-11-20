26e87de322d4dcb8051aa6f17107b1534df874e8
Adithya Paranavithana <nimsaraadithya642@gmail.com>
2025-11-20 17:58:11 +0530

    PresentAbsentDataView creation

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import SendMessageAPI, InboxAPI, SentAPI
+#from .views import SendMessageAPI, InboxAPI, SentAPI
 
 urlpatterns = [
-    path('', SendMessageAPI.as_view(), name='send-message'),
-    path('inbox/<int:user_id>/', InboxAPI.as_view(), name='inbox'),
-    path('sent/<int:user_id>/', SentAPI.as_view(), name='sent'),
+    #path('', SendMessageAPI.as_view(), name='send-message'),
+    #path('inbox/<int:user_id>/', InboxAPI.as_view(), name='inbox'),
+    #path('sent/<int:user_id>/', SentAPI.as_view(), name='sent'),
 ]
