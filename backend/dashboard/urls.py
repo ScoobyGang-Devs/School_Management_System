f5bd46e9edbc3e97dab7fb442a42378eb583333b
Selith Rubasingha <selithrubasingha@Seliths-MacBook-Pro.local>
2025-11-23 20:04:33 +0530

    commented all urls.py so it's easier to read code

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -3,5 +3,8 @@ from .views import AdminDashboardView
 
 
 urlpatterns = [
+    # used?  - gives the admin dashboard related data
     path('admin_dashboard/', AdminDashboardView.as_view(), name='admin-dashboard'),
+
+    # --> teacher_dashboard ...
 ]
