8d2126470c3a6569d1aab9e1708337829cab6269
Nayanajith Bandara <bandarashehan834@gmail.com>
2025-11-24 13:07:35 +0530

    deleted unnecessary views for change password

--- backend/SystemSettings/views.py
+++ backend/SystemSettings/views.py
@@ -8,16 +8,6 @@ from rest_framework.response import Response
 class SchoolDetailListCreateView(generics.ListCreateAPIView):
     queryset = SchoolDetail.objects.all()
     serializer_class = SchoolDetailSerializer
-
-class ChangePasswordView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request):
-        serializer = PasswordChangeSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'detail': 'Password updated successfully.'}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class AcademicCycleConfigView(generics.RetrieveUpdateAPIView):
